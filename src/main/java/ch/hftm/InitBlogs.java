2d983c36e3fb3edc4b9a605829f979aa075ff491
= <kilualthaus@hotmail.com>
2024-02-29 17:01:58 +0100

    new init blog

--- src/main/java/ch/hftm/InitBlogs.java
+++ src/main/java/ch/hftm/InitBlogs.java
@@ -20,16 +20,19 @@ public class InitBlogs {
     public void createBlogs(){
         Blog blog1 = new Blog();
         blog1.setContent("Honey Hunting");
-        blog1.setTitle("Banlgadesch Honey Hunting unterwegs in den Mangroven");
+        blog1.setTitle("Bangladesch Honey Hunting unterwegs in den Mangroven");
         Blog blog2 = new Blog();
         blog2.setTitle("Tenerifa Spanien");
         blog2.setContent("Lost places, Vulkan und Meer");
-        blog2.setValid(true);
         Blog blog3 = new Blog();
         blog3.setTitle("Gleitschirmferien in Bosnien und Herzegowina");
         blog3.setContent("Gleitschirmfliegen in Bosnien und Herzegowina");
+        Blog blog4 = new Blog();
+        blog4.setTitle("Taij Mahal in Indien");
+        blog4.setContent("Eines der sieben Weltwunder in Indien");
         blogService.create(blog1);
         blogService.create(blog2);
         blogService.create(blog3);
+        blogService.create(blog4);
     }
 }
